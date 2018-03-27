40231f639c4607d83c118bf80a87cb4506d57e8e
UAD\1302534 <necron_warrior@hotmail.co.uk>
2018-03-27 11:49:03 +0100

    little end state

---
+++ New Unity Project/Assets/Scripts/Depricated/endStateButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*	This class is a deprecated button class for the game jam 
+ * */
+public class endStateButtons : MonoBehaviour 
+{
+	public void restartLevelButton()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void returnToSplash()
+	{
+		SceneManager.LoadScene ("Splash");
+	}
+
+	public void NextLevelButton()
+	{
+		SceneManager.LoadScene ("Scene1");
+	}
+}
