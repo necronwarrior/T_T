1c8f02c130cabd2232db03a640180273b05a5276
RoryDonaldMcLean <1300060@abertay.ac.uk>
2018-03-29 15:43:15 +0100

    level 3 and camera movement
    lerp for camera moving between levels (public function so when a level
    is complete make a call to that function, will move the camera to that
    level) basic placement of objects for level 3

--- New Unity Project/Assets/Scripts/SCR_MoveCamera.cs
+++ New Unity Project/Assets/Scripts/SCR_MoveCamera.cs
@@ -16,16 +16,28 @@ public class SCR_MoveCamera : MonoBehaviour
 {
 
 	//we define this end position in the scene maybe?
-	public Vector3 EndPositionCamera;
+	//public Vector3 EndPositionCamera;
 
-	public float timeTakenDuringLerp = 1.0f;
+	float timeTakenDuringLerp = 1.0f;
 	bool _isLerping;
 	Vector3 _startPosition;
 	Vector3 _endPosition;
-
 	float _timeStartedLerping;
 
-	public void StartLerping()
+
+	//public GameObject scrollPanel;
+
+	public GameObject level1;
+	public GameObject level2;
+
+
+	Vector3 _level1Pos;
+	Vector3 _level2Pos;
+	Vector3 _level3Pos;
+
+
+
+	void StartLerping()
 	{
 		_isLerping = true;
 		_timeStartedLerping = Time.time;
@@ -33,26 +45,69 @@ public class SCR_MoveCamera : MonoBehaviour
 		//set the start position to be the cameras current position
 		//and end position to be where we define
 		_startPosition = Camera.main.transform.position;
-		_endPosition = new Vector3 (EndPositionCamera.x,
-			EndPositionCamera.y,
-			EndPositionCamera.z);
+
+
 	}
 
 
 	// Use this for initialization
 	void Start () 
 	{
-		//StartLerping ();
+
+		_level1Pos = new Vector3 (level1.transform.position.x, level1.transform.position.y, Camera.main.transform.position.z);
+		_level2Pos = new Vector3 (level2.transform.position.x, level2.transform.position.y, Camera.main.transform.position.z);
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+
+		//GetNextLevel ();
+
 		if (Input.GetKeyDown (KeyCode.Space))
 		{
+			//_endPosition = _level1Pos;
+			GetNextLevel(1);
+
 			Debug.Log ("b0ss");
 			StartLerping ();
 		}
+
+		if (Input.GetKeyDown (KeyCode.KeypadEnter))
+		{
+			_endPosition = _level2Pos;
+			StartLerping ();
+		}
+	}
+
+
+	public void GetNextLevel(int levelCount)
+	{
+		//level count would equal the next level to go to
+		//i.e if i beat level one and i want to go to level 2 then
+
+		//levelCount = next level to go to
+
+		switch (levelCount)
+		{
+		case 0:
+			_endPosition = _level1Pos;
+			break;
+
+		case 1:
+			_endPosition = _level2Pos;
+			break;
+
+		case 2:
+			break;
+
+		default:
+			Debug.Log ("Broke inside levelCount switch");
+			break;
+
+		}
+
 	}
 
 
@@ -60,10 +115,9 @@ public class SCR_MoveCamera : MonoBehaviour
 	{
 		if (_isLerping)
 		{
-			
-
 			float timeSinceStarted = Time.time - _timeStartedLerping;
 			float percentageComplete = timeSinceStarted / timeTakenDuringLerp;
+
 
 			Camera.main.transform.position = Vector3.Lerp (_startPosition, _endPosition, percentageComplete);
 
@@ -74,4 +128,5 @@ public class SCR_MoveCamera : MonoBehaviour
 
 		}
 	}
+		
 }
