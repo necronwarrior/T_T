e39de94de08b7930349297ab6084a0abf7152aab
UAD\1302534 <necron_warrior@hotmail.co.uk>
2018-03-30 16:19:20 +0100

    button implemented

--- New Unity Project/Assets/SCR_InGameObjectToggler.cs
+++ New Unity Project/Assets/SCR_InGameObjectToggler.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class SCR_InGameObjectToggler : MonoBehaviour {
 
+	public GameObject[] ActivatedObjects;
+
 	void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.gameObject.tag.Equals ("Human")) {
 			transform.position += Vector3.down;
+
+			for(int i=0; i<ActivatedObjects.Length;i++)
+			{
+				ActivatedObjects [i].SetActive (false);
+			}
 		}
 	}
 
@@ -16,6 +23,10 @@ public class SCR_InGameObjectToggler : MonoBehaviour {
 	{
 		if (collision.gameObject.tag.Equals ("Human")) {
 			transform.position += Vector3.up;
+		}
+		for(int i=0; i<ActivatedObjects.Length;i++)
+		{
+			ActivatedObjects [i].SetActive (true);
 		}
 	}
 }
